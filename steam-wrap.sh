3d5d37bc05f5a5f3b7a10b5f116a1a381374fc49
Ikey Doherty <ikey@solus-project.com>
2017-11-09 19:03:55 +0000

    Teach our shell scripts to know about 32-bit subdir drivers
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- steam-wrap.sh
+++ steam-wrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-OUR_DRIVER_PATHS="/var/lib/snapd/lib/gl:/usr/lib/glx-provider/default"
-export LIBGL_DRIVERS_PATH="$OUR_DRIVER_PATHS:/usr/lib/dri"
+OUR_DRIVER_PATHS="/var/lib/snapd/lib/gl/32:/var/lib/snapd/lib/gl:/usr/lib/glx-provider/default:/usr/lib32/glx-provider/default"
+export LIBGL_DRIVERS_PATH="$OUR_DRIVER_PATHS:/usr/lib/dri:/usr/lib32/dri"
 export LD_LIBRARY_PATH="$OUR_DRIVER_PATHS"
 
 export PATH="/bin:$PATH"
 
-exec "$SNAP"/bin/steam
+"$SNAP"/bin/steam
