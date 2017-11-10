27729f3e8d13d4aefdbf0eb526b41c42d9c6c094
Ikey Doherty <ikey@solus-project.com>
2017-11-10 00:50:01 +0000

    Let's go back to "/usr/bin" for now
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- settings-wrap.sh
+++ settings-wrap.sh
@@ -4,6 +4,4 @@ OUR_DRIVER_PATHS="/var/lib/snapd/lib/gl/32:/var/lib/snapd/lib/gl:/usr/lib/glx-pr
 export LIBGL_DRIVERS_PATH="$OUR_DRIVER_PATHS:/usr/lib/dri:/usr/lib32/dri"
 export LD_LIBRARY_PATH="$OUR_DRIVER_PATHS"
 
-export PATH="/bin:$PATH"
-
-"$SNAP"/bin/lsi-settings
+"$SNAP"/usr/bin/lsi-settings
--- steam-wrap.sh
+++ steam-wrap.sh
@@ -4,9 +4,7 @@ OUR_DRIVER_PATHS="/var/lib/snapd/lib/gl/32:/var/lib/snapd/lib/gl:/usr/lib/glx-pr
 export LIBGL_DRIVERS_PATH="$OUR_DRIVER_PATHS:/usr/lib/dri:/usr/lib32/dri"
 export LD_LIBRARY_PATH="$OUR_DRIVER_PATHS"
 
-export PATH="/bin:$PATH"
-
-# "$SNAP"/bin/steam
+# "$SNAP"/usr/bin/steam
 
 # TESTING: We need to actually make LSI know how to do this
 LD_AUDIT="$SNAP/usr/\$LIB/liblsi-intercept.so" LD_PRELOAD="$SNAP/usr/\$LIB/liblsi-redirect.so" LSI_DEBUG=1 STEAM_RUNTIME=0 DBUS_FATAL_WARNINGS=0 $SNAP/usr/lib64/steam/steam
