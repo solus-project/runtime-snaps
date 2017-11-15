3cdf7f2d9f06187a76e26465551f470561be3de2
Ikey Doherty <ikey@solus-project.com>
2017-11-15 01:42:31 +0000

    Work-around snap.yaml command line limitations
    This makes the package conformant with other snaps by adding a command
    wrapper script inside the root of the snap, which calls out to lsi-exec
    to do the real job.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- mkapp.sh
+++ mkapp.sh
@@ -21,6 +21,17 @@ function init_root()
     fi
 }
 
+# Write a wrapper out that runs the named command via our lsi-exec handler
+# and accepts commands. This is so we pass review as we cannot have compound
+# command lines within snap.yaml
+function write_wrapper()
+{
+    local shfile="$ROOTDIR/command-$1.wrapper"
+    echo "#!/bin/sh" > "$shfile"
+    echo "exec \$SNAP/bin/linux-steam-integration.exec \$SNAP/bin/$1 \$*" >> "$shfile"
+    chmod 00755 $shfile
+}
+
 # Cheap and dirty, copy the named runtime meta into the root and tell it to
 # bake a snap for us
 function cook_snap()
@@ -46,6 +57,10 @@ for i in $PACKAGE_OUT_DIR/*.eopkg ; do
 done
 
 rm -rf "$PACKAGE_OUT_DIR"
+
+for wrap in "linux-steam-integration.settings" "glxgears" "glxgears-32" "glxinfo" "glxinfo-32" ; do
+    write_wrapper $wrap
+done
 
 # Now lets cook a snap
 cook_snap linux-steam-integration
